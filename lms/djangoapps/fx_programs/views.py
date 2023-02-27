8e4f348f2ed9c8c2a4ed61feaa4950da532e3dd8
SangLM <sanglm@funix.edu.vn>
2023-02-27 08:11:05 +0700

    commit api view

--- lms/djangoapps/fx_programs/views.py
+++ lms/djangoapps/fx_programs/views.py
@@ -1,22 +1,36 @@
-from rest_framework.generics import RetrieveAPIView
 from common.djangoapps.edxmako.shortcuts import render_to_response
-
-from django.http import HttpResponse
-from django.template import loader
-import requests
+from django.http import HttpResponseServerError
 
 from cms.djangoapps.fx_programs.models import FxPrograms
 
 
 def index(request):
-    # if this is a POST request we need to process the form data
-    for program in FxPrograms.objects.all():
-        print(program.name)
-        
-    context = {
-        'status': '200',
-        'programs': FxPrograms.objects.all(),
-    }
-    response = render_to_response('programs_list.html', context)
-    return response
- 
+    try:
+        programs = FxPrograms.objects.all()
+
+        program_list = []
+
+        for program in programs:
+            metadata = program.metadata
+            for course_id in program.id_course_list.split(","):
+                if course_id:
+                    course = metadata[course_id]
+                    program_list.append({
+                        'program_name': program.name,
+                        "program_id": program.program_id,
+                        'course_id': course['id'],
+                        'course_name': course['display_name'],
+                        'language': course['language'],
+                        'course_image_url': course['course_image_url'],
+                    })
+
+        context = {
+            'status': '200',
+            'programs': program_list
+        }
+
+        print("..FUNiX Custom ..", context)
+        return render_to_response('programs_list.html', context)
+    except (ValueError, AttributeError, TypeError) as e:
+        # Bắt lỗi nếu có lỗi định dạng, thuộc tính không tồn tại hoặc giá trị không hợp lệ
+        return HttpResponseServerError('Error occurred while processing the data.', str(e))
